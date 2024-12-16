564455c6f5bc579642d6ea74498389dbec3c4061
Credgroup <54108885+Credgroup@users.noreply.github.com>
2024-12-16 16:11:36 -0300

    test docker compose

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Iniciando o processo de criação do arquivo .env
+echo "Gerando arquivo .env..."
+
+# Log de cada variável de ambiente
+echo "Valor de VITE_ENVIRONMENT_VARIABLE: ${ENVIRONMENT_VARIABLE:-development}"
+echo "Valor de VITE_IMAGE_VERSION: ${IMAGE_VERSION}"
+
+# Criando o arquivo .env com as variáveis de ambiente
+echo "VITE_ENVIRONMENT_VARIABLE=${ENVIRONMENT_VARIABLE:-development}" > /app/.env
+echo "VITE_IMAGE_VERSION=${IMAGE_VERSION}" >> /app/.env
+
+# Log indicando que o .env foi gerado
+echo ".env gerado com sucesso!"
+
+# Executando o comando da sua aplicação
+exec "$@"
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export default function App() {
+
+  const environment = import.meta.env.VITE_ENVIRONMENT_VARIABLE;
+  const imageVersion = import.meta.env.VITE_IMAGE_VERSION;
+
   return (
     <>
-      <h1>Testing sonar</h1>
+      <h1>Aplicação React com Vite</h1>
+      <p>Ambiente: {environment}</p>
+      <p>Versão da Imagem: {imageVersion}</p>
     </>
-  )
-}
+  );
+};
