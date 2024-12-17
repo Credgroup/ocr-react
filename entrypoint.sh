24852fa1c2fae8a6ba0d86d61e1de9bfb3caeaa6
Credgroup <54108885+Credgroup@users.noreply.github.com>
2024-12-17 14:13:51 -0300

    roll back

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# Defina as variáveis de ambiente que você quer no .env
-declare -A ENV_VARS=(
-  ["VITE_ENVIRONMENT_VARIABLE"]=$VITE_ENVIRONMENT_VARIABLE
-  ["VITE_IMAGE_VERSION"]=$VITE_IMAGE_VERSION
-)
+# Gera o arquivo .env a partir das variáveis do ambiente
+echo "Gerando arquivo .env a partir das variáveis do ambiente..."
+echo "VITE_ENVIRONMENT_VARIABLE=$VITE_ENVIRONMENT_VARIABLE" > .env
+echo "VITE_IMAGE_VERSION=$VITE_IMAGE_VERSION" >> .env
 
-# Função para escrever as variáveis no arquivo .env
-write_env_file() {
-  echo "Removendo arquivo .env existente, se houver, e criando um novo..."
-
-  # Remove o arquivo .env se já existir
-  rm -f .env
-
-  echo "Gerando arquivo .env a partir das variáveis do ambiente..."
-
-  # Itera sobre o array e escreve no arquivo .env
-  for var in "${!ENV_VARS[@]}"; do
-    # Verifica se a variável tem valor antes de escrever no arquivo
-    if [ -n "${ENV_VARS[$var]}" ]; then
-      echo "$var=${ENV_VARS[$var]}" >> .env
-      echo "Adicionando $var=${ENV_VARS[$var]} ao arquivo .env"
-    else
-      echo "⚠️ A variável de ambiente $var não está definida. Ignorando..."
-    fi
-  done
-
-  echo ".env gerado com sucesso:"
-  cat .env
-}
-
-# Chama a função para gerar o arquivo .env
-write_env_file
-
-# Adicional: Executa qualquer comando que tenha sido passado para o script
-exec "$@"
+echo ".env gerado com sucesso:"
+cat .env
