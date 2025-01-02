954617fc4c036dad55f4ce346f84cb5b8f4c01b3
Credgroup <54108885+Credgroup@users.noreply.github.com>
2025-01-02 11:37:43 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -91,21 +91,25 @@ export default function Home() {
       <h1>
         {enviroment} - v{appVersion}
       </h1>
-      <Webcam
-        ref={cameraRef}
-        width={300}
-        height={450}
-        screenshotFormat="image/png"
-        videoConstraints={videoConstraints}
-        className="border border-red-400"
-      />
-      <Button onClick={handleTakePhoto}>Tirar Foto</Button>
-      {photo && (
-        <img
-          src={photo}
-          alt="Preview da Foto"
-          className="w-full h-64 object-contain bg-gray-200"
-        />
+      {1 + 1 == 4 && (
+        <>
+          <Webcam
+            ref={cameraRef}
+            width={300}
+            height={450}
+            screenshotFormat="image/png"
+            videoConstraints={videoConstraints}
+            className="border border-red-400"
+          />
+          <Button onClick={handleTakePhoto}>Tirar Foto</Button>
+          {photo && (
+            <img
+              src={photo}
+              alt="Preview da Foto"
+              className="w-full h-64 object-contain bg-gray-200"
+            />
+          )}
+        </>
       )}
 
       <CameraModal isModalOpen={open} setIsModalOpen={setOpen} />
