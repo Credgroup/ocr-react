7a8249d42ae68ffe144b5b44e10115204353ec6a
Credgroup <54108885+Credgroup@users.noreply.github.com>
2025-01-09 16:40:52 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Clarity from '@microsoft/clarity';
-const projectId = import.meta.env.VITE_CLARITY_ID_KEY
+const projectId = import.meta.env.VITE_CLARITY_ID_KEY;
 
 Clarity.init(projectId);
+console.log("Projeto iniciado com id:", projectId)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
